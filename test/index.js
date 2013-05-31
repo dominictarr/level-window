437f02c3b8abbaf2b6d654169b1728f5b4e98a57
Dominic Tarr <dominic.tarr@gmail.com>
2013-05-31 21:56:42 +0200

    basic test

--- test/index.js
+++ test/index.js
@@ -1,9 +1,11 @@
-
-var level = require('level-test')()
+var opts = require('optimist').argv
+var level = require('level-test')(opts)
 var sublevel = require('level-sublevel')
 var pull = require('pull-stream')
 var pl   = require('pull-level')
 var Window = require('../')
+var window = require('pull-window')
+var timestamp = require('monotonic-timestamp')
 
 var test = require('tape')
 
@@ -15,35 +17,32 @@ test('simple', function (t) {
 
   var db = sublevel(level('window2', {encoding: 'json'}))
 
-  var windowDb = Window(db, 'window', {count: 10, initial: function () {
-      return {count: 0, sum:0, mean:0}
-  }, reduce: function (acc, value) {
-    var c = acc.count + 1, s = acc.sum + value
-    return {count: c, sum: s, mean: s / c}
-  }})
-
-  pl.read(db, {tail: true, min: '', max: '\xff\xff'})
-    .pipe(pull.drain(console.log, function (err) {
-      t.notOk(err)
+  var i = 0
+  var windowDb = Window(db, 'window',
+    window(function (data, cb) {
+      if(i++ % 5) return
+      var sum = 0, count = 0
+      return function (end, data) {
+        if(end) return
+        sum += +data.value
+        if(++count >= 20)
+          cb(null, {sum:sum, count: count, avg: sum/count})
+      }
     }))
 
-  db.once('ready', function () {
+  pull.count(100)
+    .pipe(pull.map(function (e) {
+      return {key: timestamp(), value: Math.random()}
+    }))
+    .pipe(pl.write(db, function (err) {
+      t.notOk(err)
 
-    pull.count(100)
-      .pipe(pull.map(function (e) {
-        return {key: e, value: Math.random()}
-      }))
-      .pipe(pl.write(db, function (err) {
-        t.notOk(err)
-        console.log('done')
-
-        pl.read(windowDb)
-          .pipe(pull.collect(function (err, arr) {
-            t.equal(arr.length, 10)
-            t.equal(arr.reduce(function (a, e) { return a + +e.value.count}, 0), 100)
-            t.end()
-
-          }))
-      }))
+      pl.read(windowDb)
+        .pipe(pull.collect(function (err, arr) {
+          console.log(arr)
+          
+          t.equal(arr.length, 14)
+          t.end()
+        }))
+    }))
   })
-})
